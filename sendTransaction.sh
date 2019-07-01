b854b8c959df0fda1cda216d410b6d1f50443b03
Luc Gerrits <gerrits.luc.official@gmail.com>
2019-07-01 17:26:54 +0200

    fix

--- sendTransaction.sh
+++ sendTransaction.sh
@@ -2,7 +2,7 @@
 
 echo "Running..."
 #echo -e "test" | curl -v --header "Content-Type: application/octet-stream" --raw --data-binary - --request "POST" "https://sawtooth-api.gerrits-luc.com/batches"
-./transactionTest | curl --header "Content-Type: application/octet-stream" --raw --data-binary @- --request "POST" "https://sawtooth-api.gerrits-luc.com/batches"
+./transaction -k luc -v 1 -c inc | curl --header "Content-Type: application/octet-stream" --raw --data-binary @- --request "POST" "https://sawtooth-api.gerrits-luc.com/batches"
 
 echo ""
 echo "Done."
